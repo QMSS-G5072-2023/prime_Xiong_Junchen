1221add3535ecdfd3f218e9270a8f7875bf2a9ac
JunchenXiong <jx2545@columbia.edu>
2023-10-30 12:50:11 -0400

    Edited docstrings

--- src/prime_jx2545/prime_jx2545.py
+++ src/prime_jx2545/prime_jx2545.py
@@ -1,6 +1,27 @@
 import math
 
 def is_prime(n):
+    """
+    Test if a given integer is prime.
+
+    Parameters
+    ----------
+    n : an abitrary interger inputed by the user to be tested if it is a prime or not
+
+    Returns
+    -------
+    boolean
+      True or False based on the inputed integer's primality.
+
+    Examples
+    --------
+    >>> is_prime(2)
+    True
+    >>> is_prime(4)
+    False
+    >>> is_prime(7)
+    True
+    """
     if n <= 1:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
