069f535378bfea8f202e7760adc02f66915e7022
JunchenXiong <jx2545@columbia.edu>
2023-10-30 14:02:54 -0400

    Sphinx not set up

--- tests/test_prime_jx2545.py
+++ tests/test_prime_jx2545.py
@@ -1,1 +1,12 @@
 from prime_jx2545 import prime_jx2545
+
+def test_is_prime():
+
+    # Test for known prime numbers
+    assert prime_jx2545.is_prime(2) == True
+    assert prime_jx2545.is_prime(7) == True
+    assert prime_jx2545.is_prime(11) == True
+
+    # Assert tests for known composite numbers
+    assert prime_jx2545.is_prime(8) == False
+    assert prime_jx2545.is_prime(9) == False
