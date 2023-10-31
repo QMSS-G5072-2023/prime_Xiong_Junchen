406db6c6a92ac874b80426d8c0cb401515513756
JunchenXiong <jx2545@columbia.edu>
2023-10-31 14:39:29 -0400

    Updated pytest w/ parametrization

--- tests/test_prime_jx2545.py
+++ tests/test_prime_jx2545.py
@@ -1,12 +1,26 @@
 from prime_jx2545 import prime_jx2545
 
-def test_is_prime():
+import pytest
 
-    # Test for known prime numbers
-    assert prime_jx2545.is_prime(2) == True
-    assert prime_jx2545.is_prime(7) == True
-    assert prime_jx2545.is_prime(11) == True
 
-    # Assert tests for known composite numbers
-    assert prime_jx2545.is_prime(8) == False
-    assert prime_jx2545.is_prime(9) == False
+@pytest.mark.parametrize("example, expected", [
+    # Known prime numbers
+        (2, True),
+        (3, True),
+        (5, True),
+        (7, True),
+        (11, True),
+        (13, True),
+    # known composite numbers
+        (4, False),
+        (6, False),
+        (8, False),
+        (9, False),
+        (12, False),
+    # edge cases
+        (-100, False),
+        (0, False),
+        (1, False)])
+
+def test_is_prime_param(example, expected):
+    assert prime_jx2545.is_prime(example) == expected
